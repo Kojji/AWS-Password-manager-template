936a8d44d621368cd73bfc1161112fdd60b5ad75
Kojji <fernandokojiyama@gmail.com>
2022-12-18 20:47:32 -0300

    add: base bash script

---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+guided=false
+while getopts g flag
+do
+  case "${flag}" in
+    g) guided=true;;
+  esac
+done
+if [ $guided == true ]
+then
+  echo "Running guided deployment!"
+  sam.cmd deploy --guided --capabilities CAPABILITY_NAMED_IAM --template templates/template.yaml
+else
+  echo "Running deployment!"
+  sam.cmd deploy --template templates/template.yaml
+fi
