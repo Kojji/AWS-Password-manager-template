fcb5ff95cbb369d61a8a8129ca750deec423c450
Kojji <fernandokojiyama@gmail.com>
2022-12-20 11:26:23 -0300

    add: aurora setup component

--- main.sh
+++ main.sh
@@ -9,8 +9,9 @@ done
 if [ $guided == true ]
 then
   echo "Running guided deployment!"
-  sam.cmd deploy --guided --capabilities CAPABILITY_NAMED_IAM --template templates/template.yaml
+  sam.cmd deploy --guided --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND --template templates/template.yaml
 else
   echo "Running deployment!"
   sam.cmd deploy --template templates/template.yaml
 fi
+# trigger lambda function after dedploy
