cae36413405205e40f76ca04de76bd35e4e0d619
Kojji <fernandokojiyama@gmail.com>
2022-12-20 12:51:11 -0300

    add: table creation after db provision

--- main.sh
+++ main.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Variables to set
+DBClusterName="app-test-cluster"
+createTableFunction="app-test-create-tables-function"
+
 guided=false
 while getopts g flag
 do
@@ -9,9 +13,10 @@ done
 if [ $guided == true ]
 then
   echo "Running guided deployment!"
-  sam.cmd deploy --guided --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND --template templates/template.yaml
+  sam.cmd deploy --guided --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND --template templates/template.yaml --parameter-overrides DBTableCreationFunctionName="${createTableFunction}" DBClusterName="${DBClusterName}"
+  aws lambda invoke --function-name ${createTableFunction} log-output --log-type Tail --query 'LogResult' --output text |  base64 -d
+
 else
   echo "Running deployment!"
   sam.cmd deploy --template templates/template.yaml
 fi
-# trigger lambda function after dedploy
